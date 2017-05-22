ad16918ac13f72365bb7d9166a1432b167416fbf
srmartell16 <Srmsrm691>
2017-05-22 16:26:25 -0300

    update

--- app/src/androidTest/java/com/example/srmar/cs3130_assignment2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/srmar/cs3130_assignment2/ExampleInstrumentedTest.java
@@ -2,11 +2,21 @@ package com.example.srmar.cs3130_assignment2;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.srmar.cs3130_assignment2.R.id.editText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +26,33 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.srmar.cs3130_assignment2", appContext.getPackageName());
+    private String password1;
+    private String password2;
+    Validate v1 = new Validate();
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void setString(){
+        password1 = "password";
+        password2 = "Pas5w0Rd_";
     }
+
+    @Test
+    public void passwordTest1(){
+        onView(withId(editText)).perform(typeText(password1));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Password is not strong enough!")));
+    }
+
+    @Test
+    public void passwordTest2(){
+        onView(withId(editText)).perform(typeText(password2));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Password is strong!")));
+    }
+
 }
--- app/src/main/java/com/example/srmar/cs3130_assignment2/MainActivity.java
+++ app/src/main/java/com/example/srmar/cs3130_assignment2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.srmar.cs3130_assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final Button enter = (Button) findViewById(R.id.button);
+
+        enter.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                String password = editText.getText().toString();
+                Validate v1 = new Validate();
+                if(v1.Validate(password) == true){
+                    textView.setText("Password is strong!");
+                }
+                else{
+                    textView.setText("Password is not strong enough!");
+                }
+            }
+        });
+        Button reset = (Button) findViewById(R.id.button2);
+        reset.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                textView.setText("Enter your password!");
+                editText.setText("");
+            }
+        });
     }
 }
